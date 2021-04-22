c519a91bf588e5dae18d2060a99f23fc184e2bbb
github username <github email address>
2021-04-22 15:21:35 -0400

    commit tree 49ff233dcd22d8099a5e72d09b0b54b8e17a2763

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+
+const multiply = num1 * num2
+const random = Math.round(Math.random() * 10) + 1
+let num3 = 28
+let num4 = 6
+const mod = num3 % num4
+let lo = [3,6,13,20]
+const max = Math.max(...lo)
+console.log(max)
